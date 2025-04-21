1c0fde142456629cfa64e0b11ac270c5c9db3e24
kkssbbb <03337@naver.com>
2025-04-21 17:55:30 +0900

    feat: 실습 스프링 프로젝트 초기화

---
+++ PragmaticProgramming-project/src/main/java/com/pragmaticprogramming/PragmaticProgrammingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pragmaticprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PragmaticProgrammingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PragmaticProgrammingProjectApplication.class, args);
+	}
+
+}
